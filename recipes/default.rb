435346accd7d44c1dd3977c2aa72a97de08aa874
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-11-28 12:26:17 +0000

    Add check if already installed as it throws annoying chrome error

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,28 +2,30 @@
 # Cookbook Name:: lastpass
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Protec Innovations
 #
-# All rights reserved - Do Not Redistribute
+# Distributed under BSD licence
 #
 
-remote_file "/tmp/lplinux.tar.bz2" do
-    source "https://lastpass.com/lplinux.tar.bz2"
-    mode 0644
-    checksum "691b35ab673c7d1833833e088ab1c1adfcae16182ab5b09a51c3eafd1483abf6"
-end
+unless File.exists? "/tmp/lpinstall/lpchrome_linux.crx" do
+    remote_file "/tmp/lplinux.tar.bz2" do
+        source "https://lastpass.com/lplinux.tar.bz2"
+        mode 0644
+        checksum "79f632b80cf08667439ea2a8655fc10054836b2ba0a52087ace50dbd08b72206"
+    end
 
-directory "/tmp/lpinstall" do
-    owner "root"
-    group "root"
-    mode 00755
-    action :create
-end
+    directory "/tmp/lpinstall" do
+        owner "root"
+        group "root"
+        mode 00755
+        action :create
+    end
 
-script "install-lastpass" do
-    interpreter "bash"
-    code <<-EOH
-        tar -jxf /tmp/lplinux.tar.bz2 -C /tmp/lpinstall
-        /tmp/lpinstall/install_lastpass.sh
-    EOH
+    script "install-lastpass" do
+        interpreter "bash"
+        code <<-EOH
+            tar -jxf /tmp/lplinux.tar.bz2 -C /tmp/lpinstall
+            /tmp/lpinstall/install_lastpass.sh
+        EOH
+    end
 end
